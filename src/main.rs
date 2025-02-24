e2988e1c4deaa23345a12429e08d8989cde365a6
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-02-24 09:37:09 -0500

    Use raw_data variable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use serde::Deserialize;
-use csv::ReaderBuilder;
-use std::error::Error;
 use chrono::NaiveDate;
+use csv::ReaderBuilder;
+use serde::Deserialize;
+use std::error::Error;
 
 #[derive(Debug, Deserialize)]
 struct RawData {
@@ -15,27 +15,38 @@ struct RawData {
     close: String,
 }
 
+#[derive(Debug)]
+struct CleanData {
+    date: NaiveDate,
+    close: f32,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let mut reader = ReaderBuilder::new()
         .delimiter(b'|')
         .from_path("./resources/data/historical_data.csv")?;
-    let mut data: Vec<RawData> = Vec::new();
+    let mut raw_data: Vec<RawData> = Vec::new();
 
     for result in reader.deserialize() {
         let record: RawData = result?;
-        data.push(record);
+        raw_data.push(record);
     }
 
-    let test_date_str = format!("{} {} {}", data[0].month, data[0].day, data[0].year);
-    let date = NaiveDate::parse_from_str(&test_date_str, "%b %d %Y")?;
-
-    let test_close_str = data[0].close.replace(",", "");
-    let close: f32 = test_close_str.parse()?;
+    let mut clean_data: Vec<CleanData> = Vec::new();
+    for row in &raw_data {
+        let date_str = format!("{} {} {}", row.month, row.day, row.year);
+        let date = NaiveDate::parse_from_str(&date_str, "%b %d %Y")?;
+        let close_str = row.close.replace(",", "");
+        let close: f32 = close_str.parse()?;
+        clean_data.push(CleanData { date, close });
+    }
 
     println!("The current time is: {:?}", chrono::Local::now());
-    println!("Loaded {} rows", data.len());    
-    println!("First row Date: {}", date);
-    println!("First row Close: {}", close);
+    println!("Loaded {} rows of raw data", data.len());
+    println!("Loaded {} rows of clean data", clean_data.len());
+    println!("First row Date: {}", clean_data[0].date);
+    println!("First row Close: {}", clean_data[0].close);
+    println!("First row: {:?}", clean_data[0]);
 
     Ok(())
 }
