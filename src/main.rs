eabec88141ca28a600f082814b5a436a5b7fdd37
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-02-24 09:28:08 -0500

    convert close string to f32

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use chrono::NaiveDate;
-use csv::ReaderBuilder;
 use serde::Deserialize;
+use csv::ReaderBuilder;
 use std::error::Error;
+use chrono::NaiveDate;
 
 #[derive(Debug, Deserialize)]
-struct StockData {
+struct RawData {
     #[serde(rename = "Month")]
     month: String,
     #[serde(rename = "Day")]
@@ -19,18 +19,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut reader = ReaderBuilder::new()
         .delimiter(b'|')
         .from_path("./resources/data/historical_data.csv")?;
-    let mut data: Vec<StockData> = Vec::new();
+    let mut data: Vec<RawData> = Vec::new();
 
     for result in reader.deserialize() {
-        let record: StockData = result?;
+        let record: RawData = result?;
         data.push(record);
     }
 
     let test_date_str = format!("{} {} {}", data[0].month, data[0].day, data[0].year);
     let date = NaiveDate::parse_from_str(&test_date_str, "%b %d %Y")?;
+
+    let test_close_str = data[0].close.replace(",", "");
+    let close: f32 = test_close_str.parse()?;
+
     println!("The current time is: {:?}", chrono::Local::now());
     println!("Loaded {} rows", data.len());    
     println!("First row Date: {}", date);
-    println!("First row: {:?}", data[0]);
+    println!("First row Close: {}", close);
+
     Ok(())
 }
