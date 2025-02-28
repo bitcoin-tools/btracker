b802fe2aa441db8dee9f7c2d99e888ebde885742
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-02-28 16:30:57 -0500

    ui: simplify table header into two rows (#98)

--- src/main.rs
+++ src/main.rs
@@ -136,10 +136,10 @@ impl CleanDataWithAnalytics {
             "High",
             "Low",
             "Close",
-            "Open 200-WMA",
-            "High 200-WMA",
-            "Low 200-WMA",
-            "Close 200-WMA",
+            "200_WMA_Open",
+            "200_WMA_High",
+            "200_WMA_Low",
+            "200_WMA_Close",
         ])?;
 
         data.iter().try_for_each(|row| {
@@ -445,7 +445,12 @@ fn main() -> Result<(), Box<dyn Error>> {
             <head>
                 <title>{}</title>
                 <style>
+                    th {{
+                        padding: 5px;
+                        vertical-align: bottom;
+                    }}
                     td {{
+                        padding: 5px;
                         text-align: right;
                     }}
                 </style>
@@ -462,15 +467,19 @@ fn main() -> Result<(), Box<dyn Error>> {
                 <br><br>
                 <table border='1'>
                     <tr>
-                        <th>Date</th>
+                        <th rowspan='2'>Date</th>
+                        <th colspan='4'>Daily Prices</th>
+                        <th colspan='4'>200-Week Moving Averages</th>
+                    </tr>
+                    <tr>
                         <th>Open</th>
                         <th>High</th>
                         <th>Low</th>
                         <th>Close</th>
-                        <th>Open 200-WMA</th>
-                        <th>High 200-WMA</th>
-                        <th>Low 200-WMA</th>
-                        <th>Close 200-WMA</th>
+                        <th>Open</th>
+                        <th>High</th>
+                        <th>Low</th>
+                        <th>Close</th>
                     </tr>
                     {}
                 </table>
