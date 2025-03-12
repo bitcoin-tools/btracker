277a7ed9bfd8fd8efcee8a482559151556c99501
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-03-11 20:33:52 -0400

    ui: set background color for 200WMA column

--- src/main.rs
+++ src/main.rs
@@ -512,7 +512,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     <td>{}</td>
                     <td>{}</td>
                     <td>{}</td>
-                    <td>{}</td>
+                    <td id='wmacolumn'>{}</td>
                     <td>{}</td>
                     <td>{} %</td>
                     <td>{}</td>
@@ -547,6 +547,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 <title>{CHART_TITLE}</title>
                 <link rel='icon' type='image/png' href='{OUTPUT_FAVICON_FILENAME}'>
                 <style>
+                    #wmacolumn {{
+                        background-color: blue;
+                    }}
                     img {{
                         border: 2px solid black;
                     }}
@@ -582,7 +585,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         <tr>
                             <th rowspan='2'>Date</th>
                             <th colspan='4'>Daily Prices</th>
-                            <th rowspan='2'>200-Week<br>Moving<br>Average</th>
+                            <th rowspan='2' id='wmacolumn'>200-Week<br>Moving<br>Average</th>
                             <th colspan='2'>Same-Day Swing</th>
                             <th colspan='2'>1-Day Change</th>
                             <th colspan='2'>200-Week Change</th>
