685d53353f2082a649405768a869f0ced3e54439
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-03-29 18:34:16 -0400

    clean up pull request script

--- src/util/create_pull_request.py
+++ src/util/create_pull_request.py
@@ -1,27 +1,21 @@
 from github import Github
 import os
 
-# Authenticate with GitHub
 g = Github(os.getenv("GITHUB_TOKEN"))
-
-# Get repository
 repo = g.get_repo("bitcoin-tools/btracker")  # Replace with your username/repo
 
-# Create a new branch for the update
 branch_name = "update-price"
-repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=repo.get_branch("main").commit.sha)
+repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=repo.get_branch("master").commit.sha)
 
-# Commit changes and push to the new branch
-os.system(f"git add .")
-os.system(f'git commit -m "Update price data"')
+os.system('git add .')
+os.system('git commit -m "Update price data"')
 os.system(f"git push origin {branch_name}")
 
-# Create Pull Request
 pr = repo.create_pull(
-    title="Update Price Data",
-    body="Auto-generated PR from This PR updates the price data.",
+    title="chore(data): update prices",
+    body="Auto-generated PR to update the price data.",
     head=branch_name,
-    base="main",
+    base="master",
 )
 
 print(f"Pull request created: {pr.html_url}")
