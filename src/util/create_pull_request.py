be80fed52cac944a35d7c095854afef2ceefbd9b
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-04-01 22:08:54 -0400

    feat(ci): add Python linter (#294)

--- src/util/create_pull_request.py
+++ src/util/create_pull_request.py
@@ -1,20 +1,22 @@
-from github import Github
+'''This script creates a new GitHub PR targeting master.'''
+
 import os
+from github import Github
 
 g = Github(os.getenv("GITHUB_TOKEN"))
-repo = g.get_repo("bitcoin-tools/btracker")  # Replace with your username/repo
+repo = g.get_repo("bitcoin-tools/btracker")
 
-branch_name = "update-price"
-repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=repo.get_branch("master").commit.sha)
+BRANCH_NAME = "update-price"
+repo.create_git_ref(ref=f"refs/heads/{BRANCH_NAME}", sha=repo.get_branch("master").commit.sha)
 
 os.system('git add .')
 os.system('git commit -m "Update price data"')
-os.system(f"git push origin {branch_name}")
+os.system(f"git push origin {BRANCH_NAME}")
 
 pr = repo.create_pull(
     title="chore(data): update prices",
     body="Auto-generated PR to update the price data.",
-    head=branch_name,
+    head=BRANCH_NAME,
     base="master",
 )
 
--- src/util/update_data.py
+++ src/util/update_data.py
@@ -1,46 +1,53 @@
+'''This script reads the input file, pulls new data via API, and writes back to the file.'''
+
 from datetime import datetime
 import pandas as pd
 import yfinance as yf
 
-def get_latest_data(ticker='BTC-USD', days_to_fetch=1400):
+def get_latest_data(ticker_to_check='BTC-USD', days_to_fetch=1400):
+    '''This function pulls historical data from yf'''
     print('Fetching history')
-    api_response_ticker = yf.Ticker(ticker)
-    api_response_history = api_response_ticker.history(period=f'{days_to_fetch}d', interval='1d')
-    return api_response_history
+    api_response_ticker = yf.Ticker(ticker_to_check)
+    api_response_ticker_history = api_response_ticker.history(
+        period=f'{days_to_fetch}d',
+        interval='1d'
+    )
+    return api_response_ticker_history
 
-input_data_file = 'resources/data/historical_data.tsv'
-df = pd.read_csv(input_data_file, sep='\t')
+INPUT_DATA_FILE = 'resources/data/historical_data.tsv'
+df = pd.read_csv(INPUT_DATA_FILE, sep='\t')
 
 last_record = df.iloc[0]
 last_month = last_record['Month']
-last_day = last_record['Day']
-last_year = last_record['Year']
-last_date = pd.to_datetime(f"{last_year}-{last_month}-{last_day}").tz_localize(None)
-last_open = last_record['Open']
-last_high = last_record['High']
-last_low = last_record['Low']
-last_close = last_record['Close']
-last_volume = str(last_record['Volume']).replace(',', '')
-print('Last date:', last_date.strftime('%b %d %Y'))
 print('Last month:', last_month)
+last_day = last_record['Day']
 print('Last day:', last_day)
+last_year = last_record['Year']
 print('Last year:', last_year)
+last_date = pd.to_datetime(f"{last_year}-{last_month}-{last_day}").tz_localize(None)
+print('Last date:', last_date.strftime('%b %d %Y'))
+last_open = last_record['Open']
 print('Last open:', last_open)
+last_high = last_record['High']
 print('Last high:', last_high)
+last_low = last_record['Low']
 print('Last low:', last_low)
+last_close = last_record['Close']
 print('Last close:', last_close)
-print('Last volume:', last_volume)
+# last_volume = str(last_record['Volume']).replace(',', '')
+# print('Last volume:', last_volume)
 
-ticker = 'BTC-USD'
+TICKER = 'BTC-USD'
 # Calculate the number of days between today and the last date in the file
 today = datetime.now().date()
-days_to_fetch = (today - last_date.date()).days + 2 # 2 days to account for a bug I noticed during the spring DST change in the YF API
+dynamic_days_to_fetch = (today - last_date.date()).days + 2
+# "+ 2" days to account for a bug I noticed during the spring DST change in the YF API
 print(f'Today: {today}')
-print(f'Days to fetch: {days_to_fetch}')
+print(f'Days to fetch: {dynamic_days_to_fetch}')
 
-api_response_history = get_latest_data(ticker, days_to_fetch=days_to_fetch)
+ticker_history = get_latest_data(TICKER, days_to_fetch=dynamic_days_to_fetch)
 
-latest_row_of_history = api_response_history.tail(1)
+latest_row_of_history = ticker_history.tail(1)
 latest_date = latest_row_of_history.index[0]
 latest_open = latest_row_of_history['Open']
 latest_high = latest_row_of_history['High']
@@ -57,14 +64,15 @@ print('Latest row low:', latest_low.values[0])
 print('Latest row close:', latest_close.values[0])
 print('Latest row volume:', latest_volume.values[0])
 
-for date, row in api_response_history.iterrows():
+for date, row in ticker_history.iterrows():
     date = date.tz_localize(None)
 
     if date < last_date:
         print(f"The date {date.strftime('%b %d %Y')} is already in the file.")
         continue
     if date == last_date:
-        print(f"The date {date.strftime('%b %d %Y')} is the last date. Removing before adding the new data.")
+        print(f"The date {date.strftime('%b %d %Y')} is the last date.")
+        print("Removing before adding the new data.")
         df = df.iloc[1:]
 
     print('---')
@@ -74,7 +82,7 @@ for date, row in api_response_history.iterrows():
     print(f"Low: {row['Low']}")
     print(f"Close: {row['Close']}")
     print(f"Volume: {int(row['Volume'])}")
-    
+
     new_row = {
         'Month': date.strftime('%b'),
         'Day': date.day,
@@ -91,7 +99,7 @@ for date, row in api_response_history.iterrows():
 
 print('---')
 
-output_data_file = input_data_file
-df.to_csv(output_data_file, sep='\t', index=False)
+OUTPUT_DATA_FILE = INPUT_DATA_FILE
+df.to_csv(OUTPUT_DATA_FILE, sep='\t', index=False)
 
-print('The updated data has been saved to', output_data_file)
+print(f"The updated data has been saved to {OUTPUT_DATA_FILE}")
