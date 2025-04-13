1f4f1890d85b27429787c6776383cefd05d2765c
Bitcoin Tools <156422466+bitcoin-tools@users.noreply.github.com>
2025-04-13 14:21:43 -0400

    ui: use reverse chronological order for yearly summaries (#321)

--- src/main.rs
+++ src/main.rs
@@ -487,7 +487,7 @@ impl YearlySummary {
             });
         }
 
-        yearly_summaries
+        yearly_summaries.into_iter().rev().collect()
     }
 
     fn save_to_csv(data: &[YearlySummary], path: &Path) -> Result<(), Box<dyn Error>> {
